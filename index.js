b4797558e142e047275e483a3ca4fe7ce56d5fc6
Blaze Lim <blazebentleycolim@gmail.com>
2021-05-03 23:43:40 -0700

    added main menu questions for inquirer

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 const cTable = require('console.table')
-
+const mainMenuPrompt = require('./questions/mainMenuPrompt');
---
+++ questions/mainMenuPrompt.js
@@ -0,0 +1,25 @@
+let questions = [
+    {
+        type: 'list',
+        name: 'mainMenuSelection',
+        message: 'What would you like to do?',
+        choices: [
+            "View All Departments", 
+            "View All Roles", 
+            "View All Employees", 
+            // "View All Employees By Department", 
+            // "View All Employees By Manager", 
+            "Add a Department",
+            "Add a Role",
+            "Add an Employee", 
+            // "Remove Department",
+            // "Remove Role", 
+            // "Remove Employee", 
+            "Update Employee Role"
+            // "Update Employee Manager",
+            // "View The Total Utilized Budget of a Department"
+        ]
+    }
+];
+
+module.exports = questions;
